36296343d8834aff162268b64a958545767c9f43
Altoor <Sebastian@pda.dk>
2017-04-26 12:04:35 +0200

    Nyeste opdatering bl.a. med menu

--- StepperDriv/StepperDriv.ino
+++ StepperDriv/StepperDriv.ino
@@ -10,7 +10,7 @@
 AF_Stepper motor(200*26.851, 2);
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(11, 9, 0, 1, 2, 3);
+LiquidCrystal lcd(11, 9, 3, 2, 1, 0);
 
 DS1307 clock;//define a object of DS1307 class aka. RTC
 
@@ -20,22 +20,24 @@ int motPos = 5400;
 int stepsPTick = 10;
 
 //For Input pins
-int lastinVal =0;
+int lastButton =0;
 int input1 = 0;
-int numOfButtons = 2; // buttons connected to input 1
-int buttonVals[2]= {512,730};
+int numOfButtons = 3; // buttons connected to input 1
+int buttonVals[3]= {289,509, 1020};
 
 //For All Menus
 int currMenu = 0;
-String menuNames[4] = {"main", "men1","men2","men3"};
+String menuNames[4] = {"main", "men1","men2","calibration"};
 
 //For Main Menu
 uint8_t highMenu = 0; 
 
-
 void setup() {
 
   lcd.begin(20, 4); // set up the LCD's number of columns and rows: 
+  
+  lcd.setCursor(0,0);
+  lcd.print("Test2");
   
   clock.begin(); // Setup communication w. RTC 
   motor.setSpeed(4);  // Set MotorSpeed. Works w. 4 RPM
@@ -59,53 +61,105 @@ void loop() {
   
   //printTime();
   
+  lcd.setCursor(0,0);
+  
   switch(currMenu){
     case 0: 
       MainMenu();
     break;
-    
+
+    case 3: 
+      CalibrationMenu();
+    break;
   }
+  delay(10);
 }
 
 void MainMenu(){
-  for(int i = 0; i < 4; i++){
+  for(int i = 0; i < 3; i++){
     lcd.setCursor(1,i);
     lcd.print(menuNames[i+1]);
   }
 
-  if (checkButton() == 1)    // check if button was pressed
-  {
-    lcd.setCursor(0,highMenu);
-    lcd.print(" ");
-    highMenu ++;
-  }else if(checkButton() == 2){
-    lcd.setCursor(0,highMenu);
-    lcd.print("ÆLK");
-    highMenu --;
+  switch(checkButtonDown()){
+    case 1:
+      lcd.setCursor(0,highMenu);
+      lcd.print(" ");
+      highMenu --;
+    break;
+
+    case 2: 
+      lcd.setCursor(0,highMenu);
+      lcd.print(" ");
+      highMenu ++;
+    break;
+
+    case 3: 
+      lcd.clear();
+      currMenu = highMenu-1;
+    break;
   }
+  
   highMenu %= 4; 
   
   lcd.setCursor(0,highMenu);
-  lcd.print("*");
+  lcd.print(">");
   
 }
 
+void CalibrationMenu(){
+  for(int i = 0; i < 3; i++){
+    lcd.setCursor(1,i);
+    lcd.print(menuNames[i+1]);
+  }
+
+  switch(checkButtonDown()){
+    case 1:
+      lcd.setCursor(0,highMenu);
+      lcd.print(" ");
+      highMenu --;
+    break;
+
+    case 2: 
+      lcd.setCursor(0,highMenu);
+      lcd.print(" ");
+      highMenu ++;
+    break;
+
+    case 3: 
+      lcd.clear();
+      currMenu = highMenu-1;
+    break;
+  }
+  
+  highMenu %= 4; 
+  
+  lcd.setCursor(0,highMenu);
+  lcd.print(">");
+  
+}
 
 int checkButton(){ //When a button press is detected, this value changes to said button for 1 step
-  if(lastinVal <= analogRead(input1) - 6 || lastinVal >= analogRead(input1) + 6){  
     for(int q = 0; q < numOfButtons; q ++){
       if(analogRead(input1) >= buttonVals[q] -30 && analogRead(input1) <= buttonVals[q] +30){
-        lastinVal = analogRead(input1);
         return q+1;
       }
-    }
   }
     
-  lastinVal = analogRead(input1);
   return 0;
 }
 
-void MotorUpdate(){
+int checkButtonDown(){ //When a button press is detected, this value changes to said button for 1 step
+  if(lastButton !=checkButton()){
+    lastButton = checkButton();
+    return checkButton();
+  }else {
+   return 0; 
+  }
+  
+}
+
+void MotorUpdate(){ //This updates the motor each tick, to match with the motPos value.
   int currDistance, stepsToGo;
   
   currDistance = abs(currMotPos - motPos);
